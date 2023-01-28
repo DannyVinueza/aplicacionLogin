598ec9618826201a26b86562c52ce11614d61938
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-01-28 18:28:11 -0500

    Creacion de un login

--- src/main/java/com/example/aplicacionlogin/Main.java
+++ src/main/java/com/example/aplicacionlogin/Main.java
@@ -3,6 +3,8 @@ package com.example.aplicacionlogin;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("interfaz.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
